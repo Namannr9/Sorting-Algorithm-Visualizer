ebedb715cb5813bed88058b36d755aaf2231b036
unknown <namannr9@gmail.com>
2022-07-26 11:43:31 +0530

    Updates

---
+++ precache-manifest.154ab4cb9eba3c9fa21bcebd74f7dfe1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fcd846165de91dc13987d9f425a1f1b",
+    "url": "/Sorting-Algorithm-Visualizer/index.html"
+  },
+  {
+    "revision": "38f5f5a276ef81f63c42",
+    "url": "/Sorting-Algorithm-Visualizer/static/css/2.560b9325.chunk.css"
+  },
+  {
+    "revision": "c8cbb1f96dabae06a9c5",
+    "url": "/Sorting-Algorithm-Visualizer/static/css/main.f338f545.chunk.css"
+  },
+  {
+    "revision": "38f5f5a276ef81f63c42",
+    "url": "/Sorting-Algorithm-Visualizer/static/js/2.4a626cc5.chunk.js"
+  },
+  {
+    "revision": "282cad4058e83221804611932e7ca1ed",
+    "url": "/Sorting-Algorithm-Visualizer/static/js/2.4a626cc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8cbb1f96dabae06a9c5",
+    "url": "/Sorting-Algorithm-Visualizer/static/js/main.ea5c84db.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/Sorting-Algorithm-Visualizer/static/js/main.ea5c84db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4622a06f566d5536518f",
+    "url": "/Sorting-Algorithm-Visualizer/static/js/runtime-main.98beb8b2.js"
+  }
+]);
